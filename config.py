00284bb66ef77ca4080e39b816969c53d7d081e4
Josh Moses <jmosesee@gmail.com>
2018-06-26 18:41:52 -0600

    retry

---
+++ config.py
@@ -0,0 +1,11 @@
+# keys for Bot6Plot
+consumer_key = "k9nD5XysxOSP4vl11CAHtG3zJ"
+consumer_secret = "lYiZyAUyEIOZVSg4rV8LdB07DZndZvTUNzMl22delxEq0Nqtqe"
+access_token = "1010982566550048768-pCqyarOZ39KrWx7AYPy03BqV001FtB"
+access_token_secret = "MK5VYhgYudvJEBkdao351lV90nLKgXKogRpCvvV0ReDis"
+
+# keys for JoshMos93504269
+#consumer_key = "JbYcLsXHWFJI11YGYksL9IUFd"
+#consumer_secret = "ycaBtm2CXrgFLy5wwejlaKlZrFfWnvv4q87kryBRC3XTjxvGoJ"
+#access_token = "1008558963930038278-psPTAJCtnAssAhxtkwkb1W1gKrpIK6"
+#access_token_secret = "J4UtNM0PEvzg3i4GRZ8DEE21DuOlIq349dvch0rOWjVo8"
